f4d8bb9730a3a51938d71912d5e9300890b3eb1b
Yordan Dimov <102857417+misteryco@users.noreply.github.com>
2023-12-28 18:58:38 +0200

    Instructions added

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def start_mouse_listener(mouse_position, mouse_position_lock, click_position, cl
         with mouse_position_lock:
             mouse_position['x'] = x
             mouse_position['y'] = y
-            # logging.debug(f"Mouse moved mouse_position: (X:{mouse_position['x']}, Y:{mouse_position['y']})")
+            logging.debug(f"Mouse moved mouse_position: (X:{mouse_position['x']}, Y:{mouse_position['y']})")
 
     def on_click(x, y, button, pressed):
         with click_position_lock:
@@ -52,7 +52,7 @@ def start_mouse_listener(mouse_position, mouse_position_lock, click_position, cl
                                      dict(click_position)
                                  ),)).start()
                 click_position['button'] = True
-                logging.debug("#? ###Button Clicked")
+                logging.debug("### Button Clicked")
             else:
                 click_position['button'] = False
 
@@ -65,13 +65,13 @@ async def capture_and_save_image(session, click_coords):
     ret, frame = cap.read()
 
     timestamp = datetime.now().strftime("-%d%H%M%S")
-    # image_filename = f"captured_image{timestamp}.png"
-    image_filename = os.path.join(os.path.dirname(__file__), 'images', f'captured_image{timestamp}.png')
+    f_name = f'captured_image{timestamp}.png'
+    image_filename = os.path.join(os.path.dirname(__file__), 'images', f_name)
     cv2.imwrite(image_filename, frame)
-    print(f"Image saved as {image_filename}")
+    print(f"Image saved as {f_name}")
 
     # Save the image path to the database
-    image_entry = CapturedImageModel(path=image_filename, mouse_x=click_coords['x'], mouse_y=click_coords['y'])
+    image_entry = CapturedImageModel(path=f_name, mouse_x=click_coords['x'], mouse_y=click_coords['y'])
     session.add(image_entry)
     session.commit()
 
@@ -79,7 +79,24 @@ async def capture_and_save_image(session, click_coords):
 
 
 def start_websocket_server(mouse_position, mouse_position_lock, click_position, click_position_lock):
+    """
+        Start a WebSocket server that streams current mouse position and click state.
+
+        Parameters:
+        - mouse_position (multiprocessing.managers.DictProxy): Shared dictionary for mouse position.
+        - mouse_position_lock (multiprocessing.RLock): Lock for synchronizing access to mouse_position.
+        - click_position (multiprocessing.managers.DictProxy): Shared dictionary for click position and state.
+        - click_position_lock (multiprocessing.RLock): Lock for synchronizing access to click_position.
+        """
+
     async def run_websocket_server(websocket, path):
+        """
+        Run the WebSocket server handler.
+
+        Parameters:
+        - websocket: WebSocket connection object.
+        - path: The requested WebSocket path.
+        """
         while True:
             try:
                 with mouse_position_lock and click_position_lock:
@@ -93,7 +110,13 @@ def start_websocket_server(mouse_position, mouse_position_lock, click_position, 
                     )
             except Exception as e:
                 logging.error(f"Error sending mouse position: {e}")
-            await asyncio.sleep(0.1)  # Sleep duration
+            await asyncio.sleep(0.1)
+            '''
+            Adjust above sleep duration for performance.
+            Introducing a small delay defines the rate at which the server sends updates. 
+            Preventing the while loop from running too frequently. 
+            Otherwise, the loop might run as fast as the system can handle, consuming unnecessary resources.
+            '''
 
     asyncio.get_event_loop().run_until_complete(websockets.serve(run_websocket_server, "localhost", 8765))
     asyncio.get_event_loop().run_forever()
@@ -101,11 +124,15 @@ def start_websocket_server(mouse_position, mouse_position_lock, click_position, 
 
 if __name__ == '__main__':
     with Manager() as manager:
+        # Shared dictionary's to store inter process data
         mouse_position = manager.dict({'x': 0, 'y': 0})
         click_position = manager.dict({'x': 0, 'y': 0, 'button': False})
+
+        # Locks to synchronize access to shared data
         mouse_position_lock = manager.RLock()
         click_position_lock = manager.RLock()
 
+        # Creating processes for the Mouse listener and for WebSocket server
         mouse_listener_process = multiprocessing.Process(target=start_mouse_listener,
                                                          args=(mouse_position, mouse_position_lock, click_position,
                                                                click_position_lock))
@@ -114,8 +141,10 @@ if __name__ == '__main__':
                                                            args=(mouse_position, mouse_position_lock, click_position,
                                                                  click_position_lock))
 
+        # Starting processes concurrently
         mouse_listener_process.start()
         websocket_server_process.start()
 
+        # Waiting for processes to finish
         mouse_listener_process.join()
         websocket_server_process.join()
