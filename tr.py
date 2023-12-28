50b0e72c72bfc5fa81080b5359eff72da6c817f6
Yordan Dimov <102857417+misteryco@users.noreply.github.com>
2023-12-28 19:19:37 +0200

    unnecessary file removed

--- tr.py
+++
@@ -1,7 +0,0 @@
-import os
-
-print(os.path.abspath('db_folder'))
-b = os.path.dirname(__file__)
-print(os.path.join(os.path.dirname(__file__), 'database', 'image_log.db'))
-
-print(f"sqlite:///{os.path.join(os.path.dirname(__file__), 'db', 'captured_images.db')}")
